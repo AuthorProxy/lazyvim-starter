06d76a2c87ef64cce9db1ed4ea58706f25b0aa37
Alex Kostyukov <proxyzavr@gmail.com>
2025-12-02 19:40:23 +0300

    add checkboxes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -14,7 +14,38 @@ return {
           enable = true,
         },
         markdown = {
-          headings = presets.headings.glow_center,
+          headings = presets.headings.slanted,
+          horizontal_rules = presets.horizontal_rules.dashed,
+          tables = presets.tables.rounded,
+        },
+      })
+      require("markview.extras.checkboxes").setup({
+        --- Default checkbox state(used when adding checkboxes).
+        ---@type string
+        default = "X",
+
+        --- Changes how checkboxes are removed.
+        ---@type
+        ---| "disable" Disables the checkbox.
+        ---| "checkbox" Removes the checkbox.
+        ---| "list_item" Removes the list item markers too.
+        remove_style = "disable",
+
+        --- Various checkbox states.
+        ---
+        --- States are in sets to quickly change between them
+        --- when there are a lot of states.
+        ---@type string[][]
+        states = {
+          { " ", "/", "X" },
+          { "<", ">" },
+          { "?", "!", "*" },
+          { '"' },
+          { "l", "b", "i" },
+          { "S", "I" },
+          { "p", "c" },
+          { "f", "k", "w" },
+          { "u", "d" },
         },
       })
     end,
